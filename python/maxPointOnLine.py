f745cb416e4cc1818ad285b5124b5f7d8828a3f4
ll-O-ll <ahmed.nissal@gmail.com>
2019-12-03 11:41:20 -0500

    working js

--- python/maxPointOnLine.py
+++ python/maxPointOnLine.py
@@ -9,14 +9,14 @@ def maxPointsOnLine(points):
         for j in range(i+1, len(points)):
             dx = points[j][0] - points[i][0]
             dy = points[j][1] - points[i][1]
-            if dx== 0 and dy == 0: # Case: [[0,0],[0,0]], the result should be 2. So we should count the number of same points.
+            if dx== 0 and dy == 0: # Case: [[0,0],[0,0]], the result should be 2. So we should count the number of duplicates points.
                 same_n += 1
                 continue
             gcd = getGCD(dx, dy)
             slope = str(dx//gcd) + ' ' + str(dy//gcd) # If use /, there will be '-0.0'
             dic[slope] += 1
             line_n = max(line_n, dic[slope])
-        max_n = max(max_n, line_n+same_n+1)
+        max_n = max(max_n, line_n+same_n+1) # add duplicate point counter if nonzero and add the point (i) on which we evaluate all lines that could traverse it 
     print(max_n) 
 
 def getGCD(a, b):
